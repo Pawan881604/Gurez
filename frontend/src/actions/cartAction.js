7f0a3e2618c87ef54a57f87fd0dcecf6f10ff06e
pawan <ps9502748@gmail.com>
2023-12-28 21:28:41 +0530

    first com

--- frontend/src/actions/cartAction.js
+++ frontend/src/actions/cartAction.js
@@ -9,7 +9,7 @@ export const addItemsToCart = (id, quantity) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/v1/product/${id}`);
   console.log(data)
   dispatch({
-    type: ADD_TO_CART,
+    type: ADD_TO_CART, 
     payload: {
       productId: data.Product._id,
       link: data.Product.seo[0].metalink,
